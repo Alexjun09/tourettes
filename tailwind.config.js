e67c98ac4c0d9e8d06501e79d10b691ed6bece45
alex <alex@solarlabs.gg>
2024-01-15 12:37:50 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontSize:{
+        title: "82px",
+        subtitle: "56px",
+        body: "32px"
+      },
+      fontFamily: {
+        base: ["Montserrat", "sans-serif"],
+      },
+      colors: {
+        primary: "#1D3A46",
+      },
+    },
+  },
+  plugins: [],
+};
