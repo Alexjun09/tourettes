e2898777a93230a29a669104f32b4cf47a8cde48
alex <alex@solarlabs.gg>
2024-02-27 12:06:42 +0100

    responsive mi cuenta

--- src/mi-cuenta.php
+++ src/mi-cuenta.php
@@ -79,12 +79,13 @@ $conn->close();
 </head>
 
 <body class="font-extralight grid grid-rows-[1fr_min-content] text-primary">
-    <!-- header -->
-    <header class="px-20 flex flex-row justify-between items-center py-4">
+    <!--Desktop header -->
+    <header class="hidden px-5 xl:px-20 sm:flex flex-row justify-between items-center py-4 w-full">
         <a class="h-16" href="./index.php">
-            <img src="../media/logoindex.png" alt="" class="h-full">
+            <img src="../media/logoindex.png" alt="" class="h-full hidden lg:block">
+            <img src="../media/logo.png" alt="" class="h-full lg:hidden">
         </a>
-        <nav class="flex flex-row gap-10 text-primary text-lg">
+        <nav class="flex flex-row gap-5 lg:gap-10 text-primary text-lg">
             <a href="./educacion.php">Educación</a>
             <a href="./listado-de-psicologos.php">Pedir Cita</a>
             <a href="./comunidad.php">Comunidad</a>
@@ -110,9 +111,67 @@ $conn->close();
             <!-- Otros elementos del header aquí -->
         </div>
     </header>
-    <!-- body -->
-    <div class="flex flex-col w-full items-center justify-center relative">
+    <!-- phone header -->
+    <header class="sm:hidden bg-white h-20 flex flex-row justify-between items-center p-6 sm:p-10 bg-transparent w-full z-50">
+        <a class="h-16" href="./index.php">
+            <img src="../media/logo.png" alt="" class="h-full lg:hidden">
+        </a>
+        <div class="flex">
+            <input type="checkbox" id="drawer-toggle" class="relative sr-only peer" />
+            <label for="drawer-toggle" class="top-0 left-0 inline-block transition-all duration-500 rounded-lg peer-checked:rotate-180 z-50 cursor-pointer">
+                <svg viewBox="0 0 41 27" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-10 h-10">
+                    <line y1="1" x2="41" y2="1" stroke="black" stroke-width="2"></line>
+                    <line y1="13" x2="41" y2="13" stroke="black" stroke-width="2"></line>
+                    <line y1="26" x2="41" y2="26" stroke="black" stroke-width="2"></line>
+                </svg>
+            </label>
+            <div class="fixed top-0 right-0 w-full h-full transition-all duration-500 transform translate-x-full bg-white shadow-lg peer-checked:translate-x-0 z-40">
+                <div class="w-full h-full bg-turquoise flex flex-col sm:flex-row xl:grid grid-cols-2 items-start sm:items-center px-10 sm:px-10 xl:px-60 py-40 justify-between">
+                    <div class="flex flex-col text-[24px] sm:text-header gap-4">
+                        <a class="hover:font-light hover:text-solarOrange ease-in duration-100" href="index.php">Home</a>
+                        <a class="hover:font-light hover:text-solarOrange ease-in duration-100" href="educacion.php">Educacion</a>
+                        <a class="hover:font-light hover:text-solarOrange ease-in duration-100" href="pedir-cita.php">Pedir Cita</a>
+                        <a class="hover:font-light hover:text-solarOrange ease-in duration-100" href="comunidad.php">Comunidad</a>
+                        <a class="hover:font-light hover:text-solarOrange ease-in duration-100" href="contacto.php">Contacto</a>
+                    </div>
+                    <div class="flex flex-col justify-between items-center gap-4">
+                        <!-- Otros elementos del header aquí -->
 
+                        <!-- Verificar si existe el id de paciente en la sesión -->
+                        <?php if (isset($_SESSION['idPaciente'])) : ?>
+                            <!-- Botón Mi Cuenta para usuarios logueados -->
+                            <a class="rounded-tl-xl rounded-br-xl border-br-xl bg-primary text-white py-2 px-10" href="./mi-cuenta.php">Mi Cuenta</a>
+                            <a href="#" onclick="confirmarCerrarSesion();">
+                                <img src="../media/cerrar-sesion.png" alt="Cerrar Sesión" class="rounded-tl-xl rounded-br-xl h-10">
+                            </a>
+                        <?php else : ?>
+                            <!-- Botones Sign in y Sign up para usuarios no logueados -->
+                            <a class="rounded-tl-xl rounded-br-xl border-br-xl bg-primary text-white py-2 px-6" href="./sign-in.html">Sign in</a>
+                            <a class="rounded-tl-xl rounded-br-xl border-br-xl bg-primary text-white py-2 px-6" href="./sign-up.html">Sign up</a>
+                        <?php endif; ?>
+
+                        <!-- Otros elementos del header aquí -->
+                    </div>
+                    <div class="flex justify-start sm:justify-end w-min xl:w-full">
+                        <div class="flex flex-row w-40 gap-4 items-end justify-end">
+                            <a href="">
+                                <img class="w-10 h-10" src="../media/x.png" alt="x">
+                            </a>
+                            <a href="">
+                                <img class="w-10 h-10" src="../media/insta.png" alt="insta">
+                            </a>
+                            <a href="">
+                                <img class="w-10 h-10" src="../media/facebook.png" alt="facebook">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- body -->
+    <!-- lg+ body -->
+    <div class="hidden lg:flex flex-col w-full items-center justify-center relative">
         <div class="relative flex flex-col justify-end items-center w-full">
             <img src="<?php echo $Banner; ?>" alt="" class="max-h-36 object-cover w-full">
             <div class="px-6 bg-contraste rounded-t-md absolute">
@@ -135,7 +194,7 @@ $conn->close();
                             <a class="rounded-tl-xl rounded-br-xl border-br-xl bg-primary text-white py-2 px-8 w-fit" href="./editar-mi-cuenta.php">Editar</a>
                         </div>
                     </div>
-                    <div class="w-full flex flex-row justify-start items-end min-h-[150px] relative">
+                    <div class="w-full flex flex-row justify-start items-end min-h-[150px] relative mt-20 xl:mt-0">
                         <!-- div de dani para el cita -->
                         <div class="absolute" id="citaClickada">
                         </div>
@@ -150,29 +209,68 @@ $conn->close();
                     <div class="flex flex-col gap-[20px] items-center ">
                         <p class="text-[40px]">Mis Citas</p>
                         <!-- div de dani para el calendario -->
-                        <div class="bg-white w-[60%] h-fit" id='calendar'>
+                        <div class="bg-white w-[95%] xl:w-[60%] h-fit" id='calendar'>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    <!-- lg- body -->
+    <div class="lg:hidden flex flex-col w-full items-center justify-center relative">
+        <div class="relative flex flex-col justify-end items-center w-full">
+            <img src="<?php echo $Banner; ?>" alt="" class="max-h-36 object-cover w-full">
+        </div>
+        <div class="w-full bg-contraste flex flex-col items-center justify-center  gap-10 relative py-12 px-4 sm:px-0">
+            <p class="text-[32px] sm:text-subtitle">Mi Cuenta</p>
+            <div class="h-0 w-full flex flex-row items-center justify-center translate-y-10 z-40">
+                <img src="<?php echo $FotoPerfil; ?>" alt="" class="h-36 w-36 aspect-square object-cover rounded-full border-4 border-secondary absolute z-50 bg-white">
+            </div>
+            <div class="pt-20 px-10 sm:px-20 pb-10 flex flex-col bg-secondary rounded-[30px] h-min text-white text-[24px] text-left relative gap-8 w-full sm:min-w-[400px]">
+                <div class="flex flex-col">
+                    <p><?php echo $nombreCompleto; ?></p>
+                    <p><?php echo $email; ?></p>
+                    <p>+34 <?php echo $telefonoMovil; ?></p>
+                </div>
+                <div class="w-full flex flex-row items-center justify-center">
+                    <a class="rounded-tl-xl rounded-br-xl border-br-xl bg-primary text-white py-2 px-8 w-fit" href="./editar-mi-cuenta.php">Editar</a>
+                </div>
+            </div>
+            <div class="flex flex-col w-full h-full gap-10 px-4">
+                <!-- mis citas -->
+                <div class="flex flex-col gap-[20px] items-center">
+                    <p class="text-[40px]">Mis Citas</p>
+                    <!-- div de dani para el calendario -->
+                    <div class="bg-white w-full" id="calendarResponsive">
+                    </div>
+                </div>
+            </div>
+            <div class="w-full flex flex-row justify-start items-end min-h-[150px] relative mt-20 xl:mt-0">
+                <!-- div de dani para el cita -->
+                <div class="absolute" id="citaClickadaResponsive">
+                </div>
+            </div>
+            <div class="w-full flex flex-row items-center justify-center h-fit">
+                <a class="bg-transparent border-2 border-primary px-4 sm:px-20 p-1 rounded-full w-fit text-[16px] sm:text-body" href="./mis-informes.php">Mis Informes</a>
+            </div>
+        </div>
+    </div>
     <!-- footer -->
     <br><br>
-   <footer class="h-fit w-full flex flex-col sm:flex-row bg-contraste px-4 py-4 gap-4 md:px-12 items-center justify-between">
-                <p>gLabs© 2023. Todos Los Derechos Reservados</p>
-                <div class="flex flex-row gap-4">
-                    <a href="">
-                        <img class="w-10 h-10" src="../media/x.png" alt="x">
-                    </a>
-                    <a href="">
-                        <img class="w-10 h-10" src="../media/insta.png" alt="insta">
-                    </a>
-                    <a href="">
-                        <img class="w-10 h-10" src="../media/facebook.png" alt="facebook">
-                    </a>
-                </div>
-            </footer>
+    <footer class="h-fit w-full flex flex-col sm:flex-row bg-contraste px-4 py-4 gap-4 md:px-12 items-center justify-between">
+        <p>gLabs© 2023. Todos Los Derechos Reservados</p>
+        <div class="flex flex-row gap-4">
+            <a href="">
+                <img class="w-10 h-10" src="../media/x.png" alt="x">
+            </a>
+            <a href="">
+                <img class="w-10 h-10" src="../media/insta.png" alt="insta">
+            </a>
+            <a href="">
+                <img class="w-10 h-10" src="../media/facebook.png" alt="facebook">
+            </a>
+        </div>
+    </footer>
 </body>
 
 </html>
@@ -200,7 +298,9 @@ $conn->close();
 
     document.addEventListener('DOMContentLoaded', function() {
         var calendarEl = document.getElementById('calendar');
+        var calendarResponsive = document.getElementById('calendarResponsive');
         var divCitaClickada = document.getElementById('citaClickada');
+        var divCitaClickadaResponsive = document.getElementById('citaClickadaResponsive');
         var calendar = new FullCalendar.Calendar(calendarEl, {
             initialView: 'dayGridMonth',
             events: './server/citasCalendario.php', // URL del endpoint que devuelve los eventos en JSON
@@ -215,7 +315,8 @@ $conn->close();
                 const address = info.event.extendedProps.direccion;
                 const time = info.event.start.toLocaleTimeString()
                 var details = `
-                <div id="event-details" class="grid grid-cols-4 gap-4 px-8 p-4 w-[600px] h-[150px] bg-secondary rounded-r-3xl">
+                <div id="event-details" class="flex flex-col xl:flex-row gap-4 px-8 p-4 w-fit xl:w-[600px] h-fit xl:h-[150px] bg-secondary rounded-r-3xl">
+                <div class="flex flex-row gap-4">
                     <div class="flex flex-col justify-between items-center p-4 bg-contraste rounded-3xl text-white">
                         <p class="text-5xl">${day}</p>
                         <p class="text-[24px]">${dayOfTheWeek}</p>
@@ -225,9 +326,10 @@ $conn->close();
                         <p class="text-3xl">${time.substr(0, 5)}</p>
                         <p class="text-lg">${address}</p>
                     </div>
-                    <div class="flex flex-col gap-4 h-full justify-center items-center">
-                    <a href="modificar-cita.php?psicologo_id=${idPsicologo}&cita_id=${idCita}" class="rounded-tl-xl rounded-br-xl border-br-xl bg-primary text-white py-2 px-8 w-[120px] text-center">Modificar</a>                        
-                    <a href="#" onclick="confirmarAnulacion('${idCita}');" class="rounded-tl-xl rounded-br-xl border-br-xl bg-primary text-white py-2 px-8 w-[120px] text-center">Anular</a>
+                </div>
+                    <div class="flex flex-row xl:flex-col gap-4 h-full justify-center items-center">
+                        <a href="modificar-cita.php?psicologo_id=${idPsicologo}&cita_id=${idCita}" class="rounded-tl-xl rounded-br-xl border-br-xl bg-primary text-white py-2 px-8 w-[120px] text-center">Modificar</a>                        
+                        <a href="#" onclick="confirmarAnulacion('${idCita}');" class="rounded-tl-xl rounded-br-xl border-br-xl bg-primary text-white py-2 px-8 w-[120px] text-center">Anular</a>
                     </div>
                 </div>`;
                 // Asegúrate de que solo se muestre un desplegable a la vez
@@ -240,6 +342,47 @@ $conn->close();
             }
         });
         calendar.render();
+        var calendar2 = new FullCalendar.Calendar(calendarResponsive, {
+            initialView: 'dayGridMonth',
+            events: './server/citasCalendario.php', // URL del endpoint que devuelve los eventos en JSON
+            eventColor: '#92AAB3',
+            eventClick: function(info) {
+                // Crear y mostrar un desplegable con la información del evento
+                const idCita = info.event.id; // `id` directamente contiene el ID de la cita
+                const idPsicologo = info.event.extendedProps.idPsicologo;
+                const day = new Date(info.event.start).getDate();
+                const dayOfTheWeek = dayOfTheWeekWords(new Date(info.event.start).getDay());
+                const doctor = info.event.title;
+                const address = info.event.extendedProps.direccion;
+                const time = info.event.start.toLocaleTimeString()
+                var details = `
+                <div id="event-details" class="flex flex-col xl:flex-row gap-4 sm:px-8 p-4 w-fit xl:w-[600px] h-fit xl:h-[150px] bg-secondary rounded-3xl sm:rounded-r-3xl">
+                <div class="flex flex-row gap-4">
+                    <div class="flex flex-col justify-between items-center p-4 bg-contraste rounded-3xl text-white">
+                        <p class="text-3xl sm:text-5xl">${day}</p>
+                        <p class="text-[24px]">${dayOfTheWeek}</p>
+                    </div>
+                    <div class="flex flex-col justify-between text-white col-span-2 text-xl sm:text-3xl">
+                        <p class="text-primary">${doctor}</p>
+                        <p class="">${time.substr(0, 5)}</p>
+                        <p class="text-lg">${address}</p>
+                    </div>
+                </div>
+                    <div class="flex flex-row xl:flex-col gap-4 h-full justify-center items-center">
+                        <a href="modificar-cita.php?psicologo_id=${idPsicologo}&cita_id=${idCita}" class="rounded-tl-xl rounded-br-xl border-br-xl bg-primary text-white py-2 px-8 w-[120px] text-center">Modificar</a>                        
+                        <a href="#" onclick="confirmarAnulacion('${idCita}');" class="rounded-tl-xl rounded-br-xl border-br-xl bg-primary text-white py-2 px-8 w-[120px] text-center">Anular</a>
+                    </div>
+                </div>`;
+                // Asegúrate de que solo se muestre un desplegable a la vez
+                var existingDetails = document.querySelector('#event-details');
+                if (existingDetails) {
+                    existingDetails.parentNode.removeChild(existingDetails);
+                }
+                // Insertar el nuevo desplegable
+                divCitaClickadaResponsive.insertAdjacentHTML('beforeend', details);
+            }
+        });
+        calendar2.render();
     });
 
     function confirmarAnulacion(idCita) {
@@ -279,6 +422,4 @@ $conn->close();
         });
         return false; // Evita la navegación
     }
-
-
 </script>
